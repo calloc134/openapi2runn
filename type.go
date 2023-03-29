00b68171df7942ada296aa25fb196a414366c715
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-03-29 11:21:43 +0900

    [style] ファイルを分割

---
+++ type.go
@@ -0,0 +1,27 @@
+package main
+
+// 設定ファイルの設定を格納する構造体を定義
+type config struct {
+	AllowOverride bool `toml:"allowOverride"`
+}
+
+// パラメータを格納する構造体を定義
+type paramSpec struct {
+	Name    string `json:"name"`
+	Type    string `json:"type"`
+	Example any
+}
+
+// メソッドを格納する構造体を定義
+type baseSpec struct {
+	Method string
+	Params []paramSpec
+	Body   []paramSpec
+}
+
+// パスを格納する構造体を定義
+type pathSpec struct {
+	DirName string
+	Path    string
+	Methods []baseSpec
+}
